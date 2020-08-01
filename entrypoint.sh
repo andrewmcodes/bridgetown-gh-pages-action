dff600e1526f199c06758582d3d5daf64577a2ec
Andrew Mason <andrewmcodes@protonmail.com>
2020-08-01 09:35:05 -0400

    tweak

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,40 +2,42 @@
 
 set -e
 
-echo "== Installing Dependencies =="
+REMOTE_REPO="https://${INPUT_GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git"
+git clone "$REMOTE_REPO" repo
+cd repo
 
 if [ "${INPUT_SITE_LOCATION}" != "." ]; then
   cd "${INPUT_SITE_LOCATION}"
 fi
+
+echo "== Installing Dependencies =="
 
 bundle config path vendor/bundle
 bundle install --jobs 4 --retry 3
 yarn install
 
 echo "== Running Production Build =="
-NODE_ENV=production yarn webpack-build && yarn build
+BRIDGETOWN_ENV=production NODE_ENV=production yarn webpack-build && yarn build
 
 echo "== Committing Changes =="
 cd "${INPUT_BUILD_LOCATION}"
-remote_repo="https://${INPUT_GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git" &&
-  deploy_branch=${INPUT_DEPLOY_BRANCH} &&
-  default_branch=${INPUT_DEFAULT_BRANCH}
 git init
 git config user.name "${INPUT_GITHUB_ACTOR}"
 git config user.email "${INPUT_GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 
-echo -n "Files to Commit:"
+echo "Files to Commit:"
 ls -l | wc -l
+
 echo "Committing files..."
 git commit -m "${INPUT_COMMIT_MESSAGE}" >/dev/null 2>&1
 
 echo "== Deploying =="
-echo "Pushing... to $remote_repo $default_branch:$deploy_branch"
-git push --force "$remote_repo" "$default_branch":"$deploy_branch" >/dev/null 2>&1
+echo "Pushing... to $REMOTE_REPO $INPUT_DEFAULT_BRANCH:$INPUT_DEPLOY_BRANCH"
+git push --force "$REMOTE_REPO" "$INPUT_DEFAULT_BRANCH":"$INPUT_DEPLOY_BRANCH"
 
 echo "== Cleanup =="
 rm -fr .git
-cd -
-
+cd ..
+rm -fr repo
 echo "== Done =="
