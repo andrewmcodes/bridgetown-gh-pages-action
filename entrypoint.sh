9c6eeeeba93ef4ca3d6fa18d22da8981b824df92
Andrew Mason <andrewmcodes@protonmail.com>
2020-08-01 10:13:16 -0400

    tweak output a bit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@
 # Exit immediately if a pipeline returns a non-zero status.
 set -e
 
-printf "Starting deployment action\n\n"
-
 REMOTE_REPO="https://${INPUT_GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_REPOSITORY}.git"
 git clone "$REMOTE_REPO" repo
 cd repo
@@ -13,17 +11,17 @@ if [ "${INPUT_SITE_LOCATION}" != "." ]; then
   cd "${INPUT_SITE_LOCATION}"
 fi
 
-printf "\n\nInstalling Ruby Dependencies..."
+printf "\nInstalling Ruby Dependencies..."
 bundle config path vendor/bundle
 bundle install --jobs 4 --retry 3 --quiet
 
-printf "Installing Node Dependencies..."
+printf "\nInstalling Node Dependencies..."
 yarn install --silent
 
-printf "\n\nRunning Production Build..."
+printf "\n\nRunning Production Build...\n"
 BRIDGETOWN_ENV=production NODE_ENV=production yarn webpack-build && yarn build
 
-printf "\nCommitting Changes..."
+printf "\nCommitting Changes...\n"
 cd "${INPUT_BUILD_LOCATION}"
 git init
 git config user.name "${INPUT_GITHUB_ACTOR}"
@@ -38,9 +36,10 @@ git commit -m "${INPUT_COMMIT_MESSAGE}" >/dev/null 2>&1
 
 echo "Pushing... to $REMOTE_REPO HEAD:$INPUT_DEPLOY_BRANCH"
 git push --force "$REMOTE_REPO" HEAD:"$INPUT_DEPLOY_BRANCH"
-printf "\nDeployed!"
 
-printf "\nCleanup..."
+printf "\nDeployed!\n"
+
+printf "\nCleanup...\n"
 rm -fr .git
 cd ..
 rm -fr repo
